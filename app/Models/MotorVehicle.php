effbf8a1f069ba02b4ff82ed77f1fd80a3cd599e
Kip-Rono <fieldenrono27@gmail.com>
2022-03-19 04:29:32 +0300

    modified on saving mv details

--- app/Models/MotorVehicle.php
+++ app/Models/MotorVehicle.php
@@ -10,7 +10,7 @@ class MotorVehicle extends Model
     use HasFactory;
     protected $table = 'motor_vehicle';
     public $timestamps = false;
-    public $incrementing = true;
+    public $incrementing = false;
 
     protected $fillable = [
         'id',
