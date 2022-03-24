d5b3da6e32fffbe0ea54b7bdbb3bfa5481dbed71
Kip-Rono <fieldenrono27@gmail.com>
2022-03-24 13:46:58 +0300

    modified user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
             $table->rememberToken();
             $table->timestamp('updated_at')->nullable();
             $table->timestamp('created_at')->nullable();
